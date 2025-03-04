374562d30924e4a121d264acce6d4e9b7ecab9e7
chinu2005 <chinmayimajalekar@gmail.com>
2025-03-04 09:11:02 +0530

    Add files via upload

---
+++ ROBOT.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\033[0;32m __________\n");
+printf(" |        |\n");
+printf(" | 0   0  |\n");
+printf(" |   __   |\n");
+printf(" |________|\n");
+printf(" ____| |____\n");
+printf("|           |\n");
+printf("|           |\n");
+printf("| \033[0;29m ROBOT C \033[0;33m |\n");
+printf("|           |\n");
+printf("|___________|\n");
+printf("   ||   ||  \n");
+printf("  _||   ||_  \n");
+printf(" |__|   |__|  \n");
+getch();
+}
